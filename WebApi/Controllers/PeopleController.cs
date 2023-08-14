e0d52cfa3a6bb78e7020301d142a293e10d7ecc4
milan parua <milanparua9999@gmail.com>
2023-08-14 10:56:41 +0530

    change some file

---
+++ WebApi/Controllers/PeopleController.cs
@@ -0,0 +1,94 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+namespace WebApi.Controllers
+{
+   
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PeopleController : ControllerBase
+    {
+        private static List<Employee> _employees = new List<Employee>
+    {
+        new Employee { Id = 1, FirstName = "John", LastName = "Doe", Email = "john@example.com" },
+        // ...
+    };
+
+        private static List<Student> _students = new List<Student>
+    {
+        new Student { Id = 1, FirstName = "Alice", LastName = "Johnson", RollNumber = "A123" },
+        // ...
+    };
+
+        [HttpGet("employees")]
+        public IActionResult GetEmployees()
+        {
+            return Ok(_employees);
+        }
+
+        [HttpGet("employees/{id}")]
+        public IActionResult GetEmployee(int id)
+        {
+            var employee = _employees.FirstOrDefault(e => e.Id == id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        [HttpPost("employees")]
+        public IActionResult CreateEmployee(Employee employee)
+        {
+            employee.Id = _employees.Count + 1;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
+
+            _employees.Add(employee);
+            return CreatedAtAction(nameof(GetEmployee), new { id = employee.Id }, employee);
+        }
+
+        [HttpGet("students")]
+        public IActionResult GetStudents()
+        {
+            return Ok(_students);
+        }
+
+        [HttpGet("students/{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            var student = _students.FirstOrDefault(s => s.Id == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(student);
+        }
+
+        [HttpPost("students")]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.Id = _students.Count + 1;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
+            _students.Add(student);
+            return CreatedAtAction(nameof(GetStudent), new { id = student.Id }, student);
+        }
+    }
+
+
+
+
+}
---
+++ WebApi/Person.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Student
+{
+    public int Id { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string RollNumber { get; set; }
+
+}
+public class Employee
+{
+    public int Id { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+}
